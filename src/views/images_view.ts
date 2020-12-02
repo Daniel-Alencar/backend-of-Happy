d3fd11f333c22323b2436e62f49acde0db78122d
Daniel-Alencar <danielalencar746@gmail.com>
2020-12-02 11:51:45 -0300

    up

--- src/views/images_view.ts
+++ src/views/images_view.ts
@@ -4,7 +4,7 @@ export default {
    render(image: Image) {
       return {
          id: image.id,
-         url: `http://localhost:5000/uploads/${image.path}`,
+         url: `http://10.0.0.8:5000/uploads/${image.path}`,
       };
    },
 
